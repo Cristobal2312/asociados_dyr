a36c27d1d82d6ae3bc781dafa1c5e6fbb1cc1f4e
Cristobal2312 <cristobalnahuel2002@gmail.com>
2026-03-18 15:19:59 -0300

    Fix Vue build: unify ESLint config, fix parser and rebuild project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,20 @@
-module.exports = {
+﻿module.exports = {
   root: true,
   env: {
-    node: true
-  }
-}
+    node: true,
+    browser: true,
+    es2021: true
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    requireConfigFile: false
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended'
+  ],
+  rules: {}
+};
